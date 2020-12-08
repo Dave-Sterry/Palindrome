c103faa16e8c236b1ac939f8960d18fc5798ac35
David Sterry <sterry.david@gmail.com>
2020-12-08 09:54:55 -0800

    project initialized with MSTest
    Co-authored-by: Max Brockbank <maxbrockbank1999@gmail.com>
    Co-authored-by: Diego Salazar <salazardiego343@gmail.com>

---
+++ Palindrome.Tests/ModelTests/BackwardTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestsTools.UnitTesting;
+using Palindrome;
+
+namespace Backwards.TestsTools
+{
+  [TestClass]
+  public class BackwardsTests
+  {
+    [TestMethod]
+    //test
+  }
+}
